765976097ea3f1a256584e9b852259956e3e67d2
Justin Kwan <justinkkwan@alumni.ubc.ca>
2018-02-13 21:14:55 -0800

    added search bar to portfolio

--- src/components/Dashboard/Views/Overview.vue
+++ src/components/Dashboard/Views/Overview.vue
@@ -2,7 +2,12 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        
+        <div class="filters">
+          <span>Sort By: </span>
+          <select v-model="sortBy" @change="sortProjects()">
+            <option v-for="option in sortingOptions" v-bind:key="option.value" v-bind:value="option.value">{{ option.text }}</option>
+          </select>
+        </div>
       </div>
       <div class="row">
         
@@ -14,109 +19,6 @@
           <add-project-card></add-project-card>
           
       </div>
-      <!--<div class="row">
-        <div class="col-md-8">
-          <chart-card :chart-data="lineChart.data"
-                      :chart-options="lineChart.options"
-                      :responsive-options="lineChart.responsiveOptions">
-            <template slot="header">
-              <h4 class="card-title">Users Behavior</h4>
-              <p class="card-category">24 Hours performance</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Open
-                <i class="fa fa-circle text-danger"></i> Click
-                <i class="fa fa-circle text-warning"></i> Click Second Time
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-history"></i> Updated 3 minutes ago
-              </div>
-            </template>
-          </chart-card>
-        </div>
-
-        <div class="col-md-4">
-          <chart-card :chart-data="pieChart.data" chart-type="Pie">
-            <template slot="header">
-              <h4 class="card-title">Email Statistics</h4>
-              <p class="card-category">Last Campaign Performance</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Open
-                <i class="fa fa-circle text-danger"></i> Bounce
-                <i class="fa fa-circle text-warning"></i> Unsubscribe
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-clock-o"></i> Campaign sent 2 days ago
-              </div>
-            </template>
-          </chart-card>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-6">
-          <chart-card
-            :chart-data="barChart.data"
-            :chart-options="barChart.options"
-            :chart-responsive-options="barChart.responsiveOptions"
-            chart-type="Bar">
-            <template slot="header">
-              <h4 class="card-title">2014 Sales</h4>
-              <p class="card-category">All products including Taxes</p>
-            </template>
-            <template slot="footer">
-              <div class="legend">
-                <i class="fa fa-circle text-info"></i> Tesla Model S
-                <i class="fa fa-circle text-danger"></i> BMW 5 Series
-              </div>
-              <hr>
-              <div class="stats">
-                <i class="fa fa-check"></i> Data information certified
-              </div>
-            </template>
-          </chart-card>
-        </div>
-
-        <div class="col-md-6">
-          <card>
-            <template slot="header">
-              <h5 class="title">Tasks</h5>
-              <p class="category">Backend development</p>
-            </template>
-            <l-table :data="tableData.data"
-                     :columns="tableData.columns">
-              <template slot="columns"></template>
-
-              <template slot-scope="{row}">
-                <td>
-                  <Checkbox v-model="row.checked"></Checkbox>
-                </td>
-                <td>{{row.title}}</td>
-                <td class="td-actions text-right">
-                  <button type="button" class="btn-simple btn btn-xs btn-info" v-tooltip.top-center="editTooltip">
-                    <i class="fa fa-edit"></i>
-                  </button>
-                  <button type="button" class="btn-simple btn btn-xs btn-danger" v-tooltip.top-center="deleteTooltip">
-                    <i class="fa fa-times"></i>
-                  </button>
-                </td>
-              </template>
-            </l-table>
-            <div class="footer">
-              <hr>
-              <div class="stats">
-                <i class="fa fa-history"></i> Updated 3 minutes ago
-              </div>
-            </div>
-          </card>
-
-        </div>
-      </div>-->
     </div>
   </div>
 </template>
@@ -141,88 +43,6 @@
     },
     data () {
       return {
-        editTooltip: 'Edit Task',
-        deleteTooltip: 'Remove',
-        pieChart: {
-          data: {
-            labels: ['40%', '20%', '40%'],
-            series: [40, 20, 40]
-          }
-        },
-        lineChart: {
-          data: {
-            labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-            series: [
-              [287, 385, 490, 492, 554, 586, 698, 695],
-              [67, 152, 143, 240, 287, 335, 435, 437],
-              [23, 113, 67, 108, 190, 239, 307, 308]
-            ]
-          },
-          options: {
-            low: 0,
-            high: 800,
-            showArea: false,
-            height: '245px',
-            axisX: {
-              showGrid: false
-            },
-            lineSmooth: true,
-            showLine: true,
-            showPoint: true,
-            fullWidth: true,
-            chartPadding: {
-              right: 50
-            }
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
-              }
-            }]
-          ]
-        },
-        barChart: {
-          data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-              [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-              [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-            ]
-          },
-          options: {
-            seriesBarDistance: 10,
-            axisX: {
-              showGrid: false
-            },
-            height: '245px'
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
-              }
-            }]
-          ]
-        },
-        tableData: {
-          data: [
-            {title: 'Sign contract for "What are conference organizers afraid of?"', checked: false},
-            {title: 'Lines From Great Russian Literature? Or E-mails From My Boss?', checked: true},
-            {
-              title: 'Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit',
-              checked: true
-            },
-            {title: 'Create 4 Invisible User Experiences you Never Knew About', checked: false},
-            {title: 'Read "Following makes Medium better"', checked: false},
-            {title: 'Unfollow 5 enemies from twitter', checked: false}
-          ]
-        },
         projects: [
           {
             projectId: 1,
@@ -247,12 +67,76 @@
             endDate: '2/12/2019',
             budget: 9001,
             budgetUsed: 2000
+          },
+          {
+            projectId: 3,
+            projectName: 'Top Secret Project',
+            projectStatus: 'Underway',
+            projectProgress: 90,
+            projectManager: 'Ali',
+            numPeopleOnTeam: 7,
+            startDate: '1/4/2018',
+            endDate: '4/5/2018',
+            budget: 50000,
+            budgetUsed: 23500
+          },
+          {
+            projectId: 4,
+            projectName: 'BlockChain Project (no objective)',
+            projectStatus: 'Seeking Funding',
+            projectProgress: 36,
+            projectManager: 'Daniel',
+            numPeopleOnTeam: 1,
+            startDate: '11/30/2017',
+            endDate: '8/22/2024',
+            budget: 0,
+            budgetUsed: 0
+          },
+          {
+            projectId: 5,
+            projectName: 'Make Canada Great Again',
+            projectStatus: 'Pre Approval',
+            projectProgress: 55,
+            projectManager: 'Kirsten',
+            numPeopleOnTeam: 10,
+            startDate: '12/25/2018',
+            endDate: '2/14/2020',
+            budget: 500,
+            budgetUsed: 100
           }
+        ],
+        sortBy: 'ID',
+        sortingOptions: [
+          { value: 'projectId', text: 'ID' },
+          { value: 'projectName', text: 'Name' },
+          { value: 'projectStatus', text: 'Status' },
+          { value: 'projectProgress', text: 'Completion Progress' },
+          { value: 'projectManager', text: 'Manager' },
+          { value: 'numPeopleOnTeam', text: 'Team Size' },
+          { value: 'startDate', text: 'Start Date' },
+          { value: 'endDate', text: 'End Date' },
+          { value: 'budget', text: 'Initial Budget' },
+          { value: 'budgetUsed', text: 'Budget Spent' }
         ]
+      }
+    },
+    methods: {
+      sortProjects () {
+        console.log('hergh');
+        this.projects = this.projects.sort((a,b) => { return a[this.sortBy] > b[this.sortBy] ? 1 : -1; });
       }
     }
   }
 </script>
-<style>
-
+<style scoped>
+  div.filters {
+    text-align: center;
+    width: 100%;
+    padding: 1em;
+    border: 1px solid grey;
+    border-radius: 2px;
+    margin-bottom: 1em;
+    color: white;
+    background-color: grey;
+  }
 </style>
