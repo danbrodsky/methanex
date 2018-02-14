48ec98588cb7ed06bd425231725929eeacf76cea
Daniel Brodsky <c8z9a@ugrad.cs.ubc.ca>
2018-02-14 02:30:42 -0800

    why is this a thing

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+const favicon = require('express-favicon');
+
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use(favicon(__dirname + '/static/favicon.png'));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
