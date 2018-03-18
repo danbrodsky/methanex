435ae7d2a3e9730765be21b3fb95144e7dec872e
markrp <markrepedersen@gmail.com>
2018-03-18 13:03:15 -0700

    hi

--- src/main/java/server/controller/UserController.java
+++ src/main/java/server/controller/UserController.java
@@ -1,10 +1,16 @@
 package server.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import server.model.ApplicationUser;
 import server.repository.ApplicationUserRepository;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/users")
@@ -30,6 +36,18 @@ public class UserController {
         return ResponseEntity.notFound().build();
     }
 
-
-
+//    @RequestMapping(value = {"/logout"}, method = RequestMethod.POST)
+//    public String logoutDo(HttpServletRequest request, HttpServletResponse response){
+//        HttpSession session= request.getSession(false);
+//        SecurityContextHolder.clearContext();
+//        session= request.getSession(false);
+//        if(session != null) {
+//            session.invalidate();
+//        }
+//        for(Cookie cookie : request.getCookies()) {
+//            cookie.setMaxAge(0);
+//        }
+//
+//        return "logout";
+//    }
 }
--- src/main/java/server/security/WebSecurity.java
+++ src/main/java/server/security/WebSecurity.java
@@ -35,6 +35,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf()
+                .disable()
                 .authorizeRequests()
                 .antMatchers("/**")
                 .permitAll()
