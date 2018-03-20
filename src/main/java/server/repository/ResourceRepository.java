b2919f57d1d8148bba3912e5756db802cbf89aa4
markrp <markrepedersen@gmail.com>
2018-03-19 20:16:15 -0700

    Merge branch 'heroku/shit'
    # Conflicts:
    #	.idea/workspace.xml
    #	target/SpringREST-1.0-SNAPSHOT.jar
    #	target/SpringREST-1.0-SNAPSHOT.jar.original
    #	target/classes/server/Portfolio.class
    #	target/classes/server/PortfolioRepositoryImpl.class
    #	target/classes/server/Project.class
    #	target/classes/server/ProjectController.class
    #	target/classes/server/QPortfolio.class
    #	target/classes/server/QProject.class
    #	target/classes/server/Resource.class
    #	target/generated-sources/java/server/QPortfolio.java
    #	target/generated-sources/java/server/QProject.java
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- src/main/java/server/repository/ResourceRepository.java
+++ src/main/java/server/repository/ResourceRepository.java
@@ -1,6 +1,7 @@
 package server.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import server.model.Resource;
 
-public interface ResourceRepository extends CrudRepository<Resource, Integer> {
+public interface ResourceRepository extends JpaRepository<Resource, Integer> {
 }
