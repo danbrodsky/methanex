c190e5c3bc42113db6b7333ea61ac2592ed9f6fa
markrp <markrepedersen@gmail.com>
2018-03-20 16:34:11 -0700

    Merge branch 'heroku/shit'
    # Conflicts:
    #	.idea/workspace.xml
    #	target/SpringREST-1.0-SNAPSHOT.jar
    #	target/SpringREST-1.0-SNAPSHOT.jar.original
    #	target/classes/server/Portfolio.class
    #	target/classes/server/PortfolioRepositoryImpl.class
    #	target/classes/server/Project.class
    #	target/classes/server/ProjectController.class
    #	target/classes/server/QPortfolio.class
    #	target/classes/server/QProject.class
    #	target/classes/server/Resource.class
    #	target/generated-sources/java/server/QPortfolio.java
    #	target/generated-sources/java/server/QProject.java
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- src/main/java/server/model/Category.java
+++ src/main/java/server/model/Category.java
@@ -12,4 +12,12 @@ public class Category implements Serializable {
 
     @Column(name = "name")
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
