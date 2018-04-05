793c61cd321eccf54514b1575f24f4e03e9b4497
Justin Kwan <justinkkwan@alumni.ubc.ca>
2018-04-04 20:12:00 -0700

    handle null values for statusbar

--- src/components/UIComponents/ProjectStatusBar.vue
+++ src/components/UIComponents/ProjectStatusBar.vue
@@ -9,55 +9,55 @@ export default {
     props: {
         "startDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "expectedPreApprovalStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "expectedSeekFundingStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "expectedPipelineStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "expectedConfirmedStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "expectedClosingStatusDate": {
             type: Array,
-            default() { return [2018, 12, 21] }
+            default() { return null }
         },
         "expectedClosedStatusDate": {
             type: Array,
-            default() { return [2019, 12, 21] }
+            default() { return null }
         },
         "actualPreApprovalStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "actualSeekFundingStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "actualPipelineStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "actualConfirmedStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "actualClosingStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "actualClosedStatusDate": {
             type: Array,
-            default() { return [2012, 12, 21] }
+            default() { return null }
         },
         "currentStatusPercent": {
             type: Number,
@@ -72,24 +72,40 @@ export default {
         dates: {}
     },
     mounted() {
+
+        this.startDate = [2012, 1, 1];
+        this.expectedPreApprovalStatusDate = [2012, 1, 2];
+        this.expectedSeekFundingStatusDate = [2018, 2, 2];
+        this.expectedPipelineStatusDate = [2018, 2, 3];
+        this.expectedConfirmedStatusDate = [2018, 4, 20];
+        this.expectedClosingStatusDate = [2018, 4, 22];
+        this.expectedClosedStatusDate = [2018, 4, 25];
+        this.startDate = [2012, 1, 1];
+        this.actualPreApprovalStatusDate = [2012, 1, 2];
+        this.actualSeekFundingStatusDate = [2018, 2, 2];
+        this.actualPipelineStatusDate = [2018, 2, 3];
+        this.actualConfirmedStatusDate = [2018, 4, 20];
+        this.actualClosingStatusDate = null;
+        this.actualClosedStatusDate = null;
         
+
         var target_dates = [
-            new Date(this.startDate[0], this.startDate[1], this.startDate[2], 0, 0, 0),
-            new Date(this.expectedPreApprovalStatusDate[0], this.expectedPreApprovalStatusDate[1], this.expectedPreApprovalStatusDate[2], 0, 0, 0),
-            new Date(this.expectedSeekFundingStatusDate[0], this.expectedSeekFundingStatusDate[1], this.expectedSeekFundingStatusDate[2], 0, 0, 0),
-            new Date(this.expectedPipelineStatusDate[0], this.expectedPipelineStatusDate[1], this.expectedPipelineStatusDate[2], 0, 0, 0),
-            new Date(this.expectedConfirmedStatusDate[0], this.expectedConfirmedStatusDate[1], this.expectedConfirmedStatusDate[2], 0, 0, 0),
-            new Date(this.expectedClosingStatusDate[0], this.expectedClosingStatusDate[1], this.expectedClosingStatusDate[2], 0, 0, 0),
-            new Date(this.expectedClosedStatusDate[0], this.expectedClosedStatusDate[1], this.expectedClosedStatusDate[2], 0, 0, 0)
+            this.startDate == null ? null : new Date(this.startDate[0], this.startDate[1], this.startDate[2], 0, 0, 0),
+            this.expectedPreApprovalStatusDate == null ? null : new Date(this.expectedPreApprovalStatusDate[0], this.expectedPreApprovalStatusDate[1], this.expectedPreApprovalStatusDate[2], 0, 0, 0),
+            this.expectedSeekFundingStatusDate == null ? null : new Date(this.expectedSeekFundingStatusDate[0], this.expectedSeekFundingStatusDate[1], this.expectedSeekFundingStatusDate[2], 0, 0, 0),
+            this.expectedPipelineStatusDate == null ? null : new Date(this.expectedPipelineStatusDate[0], this.expectedPipelineStatusDate[1], this.expectedPipelineStatusDate[2], 0, 0, 0),
+            this.expectedConfirmedStatusDate == null ? null : new Date(this.expectedConfirmedStatusDate[0], this.expectedConfirmedStatusDate[1], this.expectedConfirmedStatusDate[2], 0, 0, 0),
+            this.expectedClosingStatusDate == null ? null : new Date(this.expectedClosingStatusDate[0], this.expectedClosingStatusDate[1], this.expectedClosingStatusDate[2], 0, 0, 0),
+            this.expectedClosedStatusDate == null ? null : new Date(this.expectedClosedStatusDate[0], this.expectedClosedStatusDate[1], this.expectedClosedStatusDate[2], 0, 0, 0)
         ];
         var actual_dates = [
-            new Date(this.startDate[0], this.startDate[1], this.startDate[2], 0, 0, 0),
-            new Date(this. actualPreApprovalStatusDate[0], this. actualPreApprovalStatusDate[1], this. actualPreApprovalStatusDate[2], 0, 0, 0),
-            new Date(this. actualSeekFundingStatusDate[0], this. actualSeekFundingStatusDate[1], this. actualSeekFundingStatusDate[2], 0, 0, 0),
-            new Date(this. actualPipelineStatusDate[0], this. actualPipelineStatusDate[1], this. actualPipelineStatusDate[2], 0, 0, 0),
-            new Date(this. actualConfirmedStatusDate[0], this. actualConfirmedStatusDate[1], this. actualConfirmedStatusDate[2], 0, 0, 0),
-            new Date(this. actualClosingStatusDate[0], this. actualClosingStatusDate[1], this. actualClosingStatusDate[2], 0, 0, 0),
-            new Date(this. actualClosedStatusDate[0], this. actualClosedStatusDate[1], this. actualClosedStatusDate[2], 0, 0, 0)
+            this.startDate == null ? null : new Date(this.startDate[0], this.startDate[1], this.startDate[2], 0, 0, 0),
+            this.actualPreApprovalStatusDate == null ? null : new Date(this.actualPreApprovalStatusDate[0], this.actualPreApprovalStatusDate[1], this.actualPreApprovalStatusDate[2], 0, 0, 0),
+            this.actualSeekFundingStatusDate == null ? null : new Date(this.actualSeekFundingStatusDate[0], this.actualSeekFundingStatusDate[1], this.actualSeekFundingStatusDate[2], 0, 0, 0),
+            this.actualPipelineStatusDate == null ? null : new Date(this.actualPipelineStatusDate[0], this.actualPipelineStatusDate[1], this.actualPipelineStatusDate[2], 0, 0, 0),
+            this.actualConfirmedStatusDate == null ? null : new Date(this.actualConfirmedStatusDate[0], this.actualConfirmedStatusDate[1], this.actualConfirmedStatusDate[2], 0, 0, 0),
+            this.actualClosingStatusDate == null ? null : new Date(this.actualClosingStatusDate[0], this.actualClosingStatusDate[1], this.actualClosingStatusDate[2], 0, 0, 0),
+            this.actualClosedStatusDate == null ? null : new Date(this.actualClosedStatusDate[0], this.actualClosedStatusDate[1], this.actualClosedStatusDate[2], 0, 0, 0)
         ];
 
         var canvas = this.$refs.myCanvas;
@@ -109,6 +125,7 @@ export default {
 
         for(var i=0; i<target_dates.length-1; i++) {
             var tDate = target_dates[i+1]
+            if(tDate == null) break;
             if(today < tDate) {
                 var percent = Math.max(0, Math.floor(100 * (today - target_dates[i])/(target_dates[i+1] - target_dates[i])));
                 ctx.fillRect(i*100, 0, percent,50);
@@ -126,9 +143,9 @@ export default {
 
         for(var i=0; i<actual_dates.length-1; i++) {
             var aDate = actual_dates[i+1]
-            if(today < aDate) {
-                var percent = Math.max(0, Math.floor(100 * (today - actual_dates[i])/(actual_dates[i+1] - actual_dates[i])));
-                ctx.fillRect(i*100, 50, percent,50);
+            if(aDate == null) {
+                ctx.fillRect(i*100, 50, this.currentStatusPercent,50);
+                break;
             }
             else {
                 ctx.fillRect(i*100,50,100,50);
